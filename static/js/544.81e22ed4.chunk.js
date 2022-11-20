ce220f6c8a5b18f50b47f04440b6fb8085f98c4f
okarynskyi <97628955+okarynskyi@users.noreply.github.com>
2022-11-20 16:23:55 +0000

    Deploying to gh-pages from @ okarynskyi/goit-react-hw-08-phonebook@383bdaf18815d4e613f775ca722e4c947d3a9f5d 🚀

---
+++ static/js/544.81e22ed4.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[544],{544:function(e,r,n){n.r(r),n.d(r,{default:function(){return o}});var t=n(329);function o(){return(0,t.jsxs)("div",{children:[(0,t.jsx)("h2",{children:"This is your personal"}),(0,t.jsx)("h1",{children:"Phone book."}),(0,t.jsx)("h3",{children:"Register or login and save your contacts!"})]})}}}]);
+//# sourceMappingURL=544.81e22ed4.chunk.js.map
